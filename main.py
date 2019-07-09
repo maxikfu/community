972cb44c53881b4e9418272a5055449fd561f5c9
maxikfu <megorov@uh.edu>
2019-07-08 23:13:25 -0500

    added request

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ import json
 app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def processing():
-    data = json.loads(request.data)
-    if 'type' not in data.keys:
-        return 'not from vk'
-    if data['type'] == 'confirmation':
-        return '85b0d4a5'
-    else:
-        return 'ok'
+    return '85b0d4a5'
 
 
 if __name__ == '__main__':
